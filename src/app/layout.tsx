c3fb348c346f7efe08d02e6bd26fbead42cfa6b3
Érick Nilson Souza Sodré Filho <erick@desenlike.com.br>
2025-03-07 11:51:59 -0300

    layout added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,21 @@
-// src/app/layout.tsx
-import '../styles/globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Webhook Inspector",
+  description: "Inpspect incoming webhooks",
+};
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -7,7 +23,11 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         <title>Webhook Inspector</title>
       </head>
-      <body>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+          {children}
+      </body>
     </html>
   );
 }
